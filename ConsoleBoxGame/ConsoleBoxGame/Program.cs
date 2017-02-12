6138bba6bf654902d98d7d0854a37318aec9a833
Marina <miramarrina@gmail.com>
2017-02-12 23:42:08 +0200

    Add DuelGame

---
+++ ConsoleBoxGame/ConsoleBoxGame/Program.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Resources;
+using System.Drawing;
+using System.Threading;
+
+namespace ConsoleBoxGame
+{
+    class Program {
+        static void Main(string[] args) {
+            Game game = new Game();
+            while (game.loop() == 0) {}
+            Console.Clear();
+
+            if (game.gameResult == 1) {
+                Console.Write("GAME OVER! YOU LOSE");
+            } else {
+                Console.Write("GAME OVER! YOU WIN");
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class Game {
+        static void write(String text) {
+            Console.Write(text);
+        }
+        static void writeln(String text) {
+            Console.WriteLine(text);
+        }
+
+        static int inputDigitals() {
+            var userInput = Console.ReadKey();
+
+            if (!char.IsDigit(userInput.KeyChar)) {
+                return -1;
+            }
+            int n = -1;
+            var isParsed = int.TryParse(userInput.KeyChar.ToString(), out n);
+
+            return isParsed ? n : -1;
+        }
+
+
+        Bitmap[] topAtackAnimtions() {
+            Bitmap[] bitmaps = {
+                ConsoleBoxGame.Properties.assets.SpriteP1Top1,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top2,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top3,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top4,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top5,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top6,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top7,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top8,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top9,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top10,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top11,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top12,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top13,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top14,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top15,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top16,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top17,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top18,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top19,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top20,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top21,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top22,
+                ConsoleBoxGame.Properties.assets.SpriteP1Top23
+            };
+            return bitmaps;
+        }
+        Bitmap[] bottomAtackAnimtions() {
+            Bitmap[] bitmaps = {
+                ConsoleBoxGame.Properties.assets.SpriteP1Bottom1,
+                ConsoleBoxGame.Properties.assets.SpriteP1Bottom2,
+                ConsoleBoxGame.Properties.assets.SpriteP1Bottom3,
+                ConsoleBoxGame.Properties.assets.SpriteP1Bottom4,
+                ConsoleBoxGame.Properties.assets.SpriteP1Bottom5,
+                ConsoleBoxGame.Properties.assets.SpriteP1Bottom6,
+                ConsoleBoxGame.Properties.assets.SpriteP1Bottom7,
+                ConsoleBoxGame.Properties.assets.SpriteP1Bottom8
+            };
+            return bitmaps;
+        }
+        Bitmap[] middleAtackAnimtions() {
+            Bitmap[] bitmaps = {
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle1,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle2,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle3,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle4,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle5,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle6,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle7,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle8,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle9,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle10,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle11,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle12,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle13,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle14,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle15,
+                ConsoleBoxGame.Properties.assets.SpriteP1Middle16
+
+            };
+            return bitmaps;
+        }
+
+        // ---------------------------------------------------------
+
+        enum Body {
+            head = 1,
+            body = 2,
+            leg = 3
+        }
+        enum Player {
+            player = 1,
+            bot = 2
+        }
+
+        int player1Hp = 10;
+        int botHp = 10;
+        Body player1AtackType = Body.head;
+        Body botAtackType = Body.head;
+        Body player1EvadeType = Body.head;
+        Body botEvadeType = Body.head;
+        public int gameResult = 0;
+
+        public Game() {
+            var launchScreen = ConsoleBoxGame.Properties.assets.Menu.ScaleBitmap(0.7f).ASCIIFilter(5,5);
+            write(launchScreen);
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        int choseAtack() {
+            Console.Clear();
+            write("Chose atack type:\n 1. HEAD\n 2. Body\n 3. Leg\n");
+            var type = inputDigitals();
+            return type;
+        }
+
+        int choseEvade() {
+            Console.Clear();
+            write("Chose evade type:\n 1. HEAD\n 2. Body\n 3. Leg\n");
+            var type = inputDigitals();
+            return type;
+        }
+
+        void animatePlayer1AtackByType(Body type, Player player) {
+            switch (type) {
+                case Body.head: animateBitmaps(topAtackAnimtions(), player); break;
+                case Body.body: animateBitmaps(middleAtackAnimtions(), player); break;
+                case Body.leg:  animateBitmaps(bottomAtackAnimtions(), player); break;
+            }
+        }
+
+        void animateBitmaps(Bitmap[] bitmaps, Player player) {
+            for (int i = 0; i < bitmaps.Length; i++) {
+                Console.Clear();
+                var bitmap = bitmaps[i];
+
+                if (player == Player.bot) {
+                    bitmap.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                }
+                var ascii = bitmap.ScaleBitmap(0.7f).ASCIIFilter(5, 10);
+                write(ascii);
+                Thread.Sleep(100);
+            }
+        }
+
+        public int loop() {
+            while (true) {
+                var type = choseAtack();
+                if (type != -1 && type > 0 && type < 4) {
+                    player1AtackType = (Body)type;
+                    break;
+                }
+                Console.Clear();
+                write("Incorrect value. Choose from 1 2 3 Range");
+                Console.ReadKey();
+            }
+
+            while (true) {
+                var type = choseEvade();
+                if (type != -1 && type > 0 && type < 4) {
+                    player1EvadeType = (Body)type;
+                    break;
+                }
+                Console.Clear();
+                write("Incorrect value. Choose from 1 2 3 Range");
+                Console.ReadKey();
+            }
+
+            Random random = new Random();
+            botAtackType = (Body)random.Next(1, 3);
+            botEvadeType = (Body)random.Next(1, 3);
+
+            animatePlayer1AtackByType(player1AtackType, Player.player);
+            Console.Clear();
+
+            if (player1AtackType != botEvadeType) {
+                botHp -= 1;
+                write("Your atack sucess - BOT HP (" + botHp + ")");
+            } else {
+                write("Your atack evaded - BOT HP (" + botHp + ")");
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+            animatePlayer1AtackByType(botAtackType, Player.bot);
+
+            Console.Clear();
+            if (botAtackType != player1EvadeType) {
+                player1Hp -= 1;
+                write("Bot atack sucess - PLAYER HP (" + player1Hp + ")");
+            } else {
+                write("Bot atack evaded - PLAYER HP (" + player1Hp + ")");
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+            if (botHp == 0) {
+                gameResult = 1;
+                return -1;
+            }
+
+            if (player1Hp == 0) {
+                gameResult = 2;
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ ConsoleBoxGame/ConsoleBoxGame/SpriteEngine.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+
+namespace ConsoleBoxGame {
+    public static class ExtBitmap {
+        public static string ASCIIFilter(this Bitmap sourceBitmap, int pixelBlockSize,
+                                                                   int colorCount = 0) {
+            BitmapData sourceData = sourceBitmap.LockBits(new Rectangle(0, 0,
+                                    sourceBitmap.Width, sourceBitmap.Height),
+                                                      ImageLockMode.ReadOnly,
+                                                PixelFormat.Format32bppArgb);
+
+            byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+            sourceBitmap.UnlockBits(sourceData);
+
+            StringBuilder asciiArt = new StringBuilder();
+
+            int avgBlue = 0;
+            int avgGreen = 0;
+            int avgRed = 0;
+            int offset = 0;
+
+            int rows = sourceBitmap.Height / pixelBlockSize;
+            int columns = sourceBitmap.Width / pixelBlockSize;
+
+            if (colorCount > 0) {
+                colorCharacters = "_-=<>#";//"+-!~%&@=#$";//GenerateRandomString(colorCount);
+            }
+
+            for (int y = 0; y < rows; y++) {
+                for (int x = 0; x < columns; x++) {
+                    avgBlue = 0;
+                    avgGreen = 0;
+                    avgRed = 0;
+
+                    for (int pY = 0; pY < pixelBlockSize; pY++) {
+                        for (int pX = 0; pX < pixelBlockSize; pX++) {
+                            offset = y * pixelBlockSize * sourceData.Stride +
+                                     x * pixelBlockSize * 4;
+
+                            offset += pY * sourceData.Stride;
+                            offset += pX * 4;
+
+                            avgBlue += pixelBuffer[offset];
+                            avgGreen += pixelBuffer[offset + 1];
+                            avgRed += pixelBuffer[offset + 2];
+                        }
+                    }
+
+                    avgBlue = avgBlue / (pixelBlockSize * pixelBlockSize);
+                    avgGreen = avgGreen / (pixelBlockSize * pixelBlockSize);
+                    avgRed = avgRed / (pixelBlockSize * pixelBlockSize);
+
+                    asciiArt.Append(GetColorCharacter(avgBlue, avgGreen, avgRed));
+                }
+
+                asciiArt.Append("\r\n");
+            }
+
+            return asciiArt.ToString();
+        }
+
+        private static string colorCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        private static string GetColorCharacter(int blue, int green, int red) {
+            string colorChar = String.Empty;
+            int intensity = (blue + green + red) / 3 *
+                            (colorCharacters.Length - 1) / 255;
+
+            colorChar = colorCharacters.Substring(intensity, 1).ToUpper();
+            colorChar += colorChar.ToLower();
+
+            return colorChar;
+        }
+
+        public static Bitmap ScaleBitmap(this Bitmap sourceBitmap, float factor) {
+            Bitmap resultBitmap = new Bitmap((int)(sourceBitmap.Width * factor),
+                                             (int)(sourceBitmap.Height * factor),
+                                             PixelFormat.Format32bppArgb);
+
+            Graphics graphics = Graphics.FromImage(resultBitmap);
+
+            graphics.CompositingQuality = CompositingQuality.HighQuality;
+            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+
+            graphics.DrawImage(sourceBitmap,
+                new Rectangle(0, 0, resultBitmap.Width, resultBitmap.Height),
+                new Rectangle(0, 0, sourceBitmap.Width, sourceBitmap.Height),
+                                                         GraphicsUnit.Pixel);
+
+            return resultBitmap;
+        }
+    }
+}
